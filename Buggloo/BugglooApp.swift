8ea21cd8d42aeac6b0d778e0b71875bfb7ea1e5e
Florin Stroe <florin@stroe.dev>
2025-04-26 19:23:49 +0300

    Initial Commit

---
+++ Buggloo/BugglooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugglooApp.swift
+//  Buggloo
+//
+//  Created by Florin Stroe on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BugglooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
